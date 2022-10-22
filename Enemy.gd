371d99836662b2e8311fac6b58f51795a70407db
Mikołaj Lubiak <mikolajlubiak@danwin1210.de>
2022-10-22 13:23:07 +0200

    cos

--- Enemy.gd
+++ Enemy.gd
@@ -11,21 +11,17 @@ func _physics_process(delta):
 	look_at(Player.position)
 	move_and_collide(motion)
 
-func spawn():
-	var rand = RandomNumberGenerator.new()
-	var enemyscene = load("res://Enemy.tscn")
-	var screen_size = get_viewport().get_visible_rect().size
-	var enemy = enemyscene.instance()
-
-spawn()
-
 func _on_Area2D_body_entered(body):
 	if "Bullet" in body.name:
-	queue_free()
-	rand.randomize()
-	var x = rand.randf_range(0, screen_size.x)
-	rand.randomize()
-	var y = rand.randf_range(0, screen_size.y)
-	enemy.position.y = y
-	enemy.position.x = x
-	add_child(enemy)
+		queue_free()
+		var rand = RandomNumberGenerator.new()
+		var enemyscene = load("res://Enemy.tscn")
+		var screen_size = get_viewport().get_visible_rect().size
+		var enemy = enemyscene.instance()
+		rand.randomize()
+		var x = rand.randf_range(0, screen_size.x)
+		rand.randomize()
+		var y = rand.randf_range(0, screen_size.y)
+		enemy.position.y = y
+		enemy.position.x = x
+		add_child(enemy)
